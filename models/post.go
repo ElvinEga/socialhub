2122a30fa32d125dfe07094f8c5e3d7537a3fe1a
ElvinEga <proega3@gmail.com>
2025-04-01 12:04:35 +0300

    Replace image_urls with Media model in Post struct

--- models/post.go
+++ models/post.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type Post struct {
-	ID            uint      `json:"id" gorm:"primaryKey"`
-	Content       string    `json:"content"`
-	ImageUrls     []string  `json:"image_urls" gorm:"type:text[]"`
+	ID      uint   `json:"id" gorm:"primaryKey"`
+	Content string `json:"content"`
+
+	Media         []Media   `json:"media" gorm:"foreignKey:PostID"`
 	UserID        uint      `json:"user_id"`
 	User          User      `json:"user" gorm:"foreignKey:UserID"`
-	Likes         []Like    `json:"likes"`
-	Comments      []Comment `json:"comments"`
+	Likes         []Like    `json:"likes" gorm:"foreignKey:PostID"`
+	Comments      []Comment `json:"comments" gorm:"foreignKey:PostID"`
 	PostType      string    `gorm:"default:'regular'" json:"post_type"`
 	CommentsCount int64     `json:"comments_count" gorm:"default:0"`
 	LikeCount     int64     `json:"likes_count" gorm:"default:0"`
