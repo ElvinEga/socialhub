8020bc8de98f323a20f77cf2db2088f18a6d5f21
ElvinEga <proega3@gmail.com>
2025-04-01 12:35:34 +0300

    Extend User model with social counts and relationships

--- models/user.go
+++ models/user.go
@@ -1,20 +1,23 @@
 package models
 
 import (
-	"time"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	ID             uint      `gorm:"primaryKey" json:"id"`
-	Email          string    `gorm:"unique;not null" json:"email"`
-	Password       string    `json:"-"` // omit in JSON responses
-	ProfilePicture string    `json:"profile_picture"`
-	Name           string    `json:"name"`
-	Username       string    `gorm:"unique" json:"username"`
-	Bio            string    `json:"bio"`
-	CreatedAt      time.Time `json:"created_at"`
+	gorm.Model
+	Email          string `gorm:"unique;not null" json:"email"`
+	Password       string `json:"-"` // omit in JSON responses
+	ProfilePicture string `json:"profile_picture"`
+	Name           string `json:"name"`
+	Username       string `gorm:"unique" json:"username"`
+	Bio            string `json:"bio"`
+	FollowerCount  int64  `json:"follower_count" gorm:"default:0"`
+	FollowingCount int64  `json:"following_count" gorm:"default:0"`
 
-	Posts []Post `json:"posts"`
+	Posts    []Post    `json:"posts" gorm:"foreignKey:UserID"`
+	Comments []Comment `json:"comments" gorm:"foreignKey:UserID"`
+	Likes    []Like    `json:"likes" gorm:"foreignKey:UserID"`
 	// Self-referential many-to-many for followers and following.
 	Followers []*User `gorm:"many2many:follows;joinForeignKey:FollowingID;joinReferences:FollowerID" json:"followers"`
 	Following []*User `gorm:"many2many:follows;joinForeignKey:FollowerID;joinReferences:FollowingID" json:"following"`
