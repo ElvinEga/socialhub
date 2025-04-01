0ec89789f5b496b299b69a88d0358b3eee2e74e9
ElvinEga <proega3@gmail.com>
2025-04-01 12:29:59 +0300

    Update Follow model to include user relationships

--- models/follow.go
+++ models/follow.go
@@ -1,10 +1,14 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+)
 
-// Follow represents the join table for user follow relationships.
 type Follow struct {
-	FollowerID  uint      `gorm:"primaryKey" json:"follower_id"`
-	FollowingID uint      `gorm:"primaryKey" json:"following_id"`
-	CreatedAt   time.Time `json:"created_at"`
+	gorm.Model
+	FollowerID  uint `gorm:"primaryKey" json:"follower_id"`
+	FollowingID uint `gorm:"primaryKey" json:"following_id"`
+
+	Followers User `gorm:"foreignKey:FollowerID" json:"follower"`
+	Following User `gorm:"foreignKey:FollowingID" json:"following"`
 }
