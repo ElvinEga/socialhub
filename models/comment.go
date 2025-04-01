186cc54c1bf8f66c313ac104e5b7919bd5b5c374
ElvinEga <proega3@gmail.com>
2025-04-01 12:22:57 +0300

    Add comment likes and improve model relationships

--- models/comment.go
+++ models/comment.go
@@ -3,13 +3,19 @@ package models
 import "time"
 
 type Comment struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Content   string    `gorm:"type:text" json:"content"`
-	UserID    uint      `json:"user_id"`
-	User      User      `json:"user"`
-	ParentID  *uint     `json:"parent_id,omitempty"`
-	Replies   []Comment `gorm:"foreignKey:ParentID" json:"replies,omitempty"`
-	PostID    uint      `json:"post_id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID              uint   `gorm:"primaryKey" json:"id"`
+	Content         string `gorm:"type:text" json:"content"`
+	UserID          uint   `json:"user_id"`
+	ParentCommentID *uint  `json:"parent_id,omitempty"`
+	PostID          uint   `json:"post_id"`
+	LikeCount       int64  `json:"likes_count" gorm:"default:0"`
+
+	User          User      `json:"user" gorm:"foreignKey:UserID"`
+	Post          Post      `json:"post" gorm:"foreignKey:PostID"`
+	ParentComment *Comment  `json:"parent,omitempty" gorm:"foreignKey:ParentCommentID"`
+	Replies       []Comment `gorm:"foreignKey:ParentCommentID" json:"replies,omitempty"`
+	Likes         []Like    `json:"likes" gorm:"foreignKey:CommentID"`
+	IsLiked       bool      `json:"is_liked" gorm:"-"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
--- models/like.go
+++ models/like.go
@@ -3,9 +3,13 @@ package models
 import "time"
 
 type Like struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	UserID    uint      `json:"user_id"`
-	User      User      `json:"user"`
-	PostID    uint      `json:"post_id"`
+	ID        uint  `gorm:"primaryKey" json:"id"`
+	UserID    uint  `json:"user_id"`
+	PostID    *uint `json:"post_id"`
+	CommentID *uint `json:"comment_id"`
+
+	User      User      `json:"user" gorm:"foreignKey:UserID"`
+	Post      *Post     `json:"post,omitempty" gorm:"foreignKey:PostID"`
+	Comment   *Comment  `json:"comment,omitempty" gorm:"foreignKey:CommentID"`
 	CreatedAt time.Time `json:"created_at"`
 }
